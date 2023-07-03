89d96b30fbc974cb41491d666c94f0f635df3c27
ayleeshermeesherm <alixsherman.coding@gmail.com>
2023-07-03 09:34:06 +0100

    new file added - turtle_fun.py

---
+++ turtle_fun.py
@@ -0,0 +1,13 @@
+import turtle
+window = turtle.Screen()
+window.title("Turtle Circle")
+
+bob = turtle.Turtle()
+bob.shape("turtle")
+bob.color('green')
+bob.pensize(3)
+bob.speed(8)
+
+bob.circle(50)
+
+window.exitonclick()
